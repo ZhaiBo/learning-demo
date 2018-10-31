499f5d70f62fc3f8d50e4f3cebefb61614815440
zhaibo <729963917@qq.com>
2018-10-31 18:44:54 +0800

    Netty Demo.

--- netty/src/main/java/ink/zhaibo/netty/NettyClient.java
+++ netty/src/main/java/ink/zhaibo/netty/NettyClient.java
@@ -9,14 +9,22 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class NettyClient {
+
+    private static final int MAX_RETRY = 5;
+
     public static void main(String[] args) throws Exception {
         Bootstrap bootstrap = new Bootstrap();
         NioEventLoopGroup group = new NioEventLoopGroup();
 
-        bootstrap.group(group)
+        bootstrap
+                // 1.指定线程模型
+                .group(group)
+                // 2.指定 IO 类型为 NIO
                 .channel(NioSocketChannel.class)
+                // 3.IO 处理逻辑
                 .handler(new ChannelInitializer<Channel>() {
                     @Override
                     protected void initChannel(Channel ch) {
@@ -25,10 +33,29 @@ public class NettyClient {
                 });
 
         Channel channel = bootstrap.connect(Constants.LOCAL_HOST, Constants.SERVER_PORT).channel();
-
         while (true) {
             channel.writeAndFlush(new Date() + ": hello world!");
             Thread.sleep(2000);
         }
+
+        //connect(bootstrap,Constants.LOCAL_HOST,Constants.SERVER_PORT,MAX_RETRY);
+    }
+
+    private static void connect(Bootstrap bootstrap, String host, int port, int retry) {
+        bootstrap.connect(host, port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功!");
+            } else if (retry == 0) {
+                System.err.println("重试次数已用完，放弃连接！");
+            } else {
+                // 第几次重连
+                int order = (MAX_RETRY - retry) + 1;
+                // 本次重连的间隔
+                int delay = 1 << order;
+                System.err.println(new Date() + ": 连接失败，第" + order + "次重连……");
+                bootstrap.config().group().schedule(() -> connect(bootstrap, host, port, retry - 1), delay, TimeUnit
+                        .SECONDS);
+            }
+        });
     }
 }

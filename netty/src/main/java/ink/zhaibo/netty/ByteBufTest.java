65fd9b550db3e502b9d6a5f23b1e1e1bfd7843d7
zhaibo <729963917@qq.com>
2018-11-01 18:25:33 +0800

    Netty Demo.

--- netty/src/main/java/ink/zhaibo/netty/ByteBufTest.java
+++ netty/src/main/java/ink/zhaibo/netty/ByteBufTest.java
@@ -3,6 +3,9 @@ package ink.zhaibo.netty;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 
+/**
+ * ByteBuf Api
+ */
 public class ByteBufTest {
     public static void main(String[] args) {
         ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer(9, 100);
